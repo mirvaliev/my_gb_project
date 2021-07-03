5d56e1bf2e1a60f0b87c3832088a30106c68765d
mirvaliev <sx.mirvaliev@gmail.com>
2021-07-03 19:55:40 +0500

    Прямоугольный триугольник

---
+++ Documents/XCP/HW_1.2_Mirvaliev.playground/Contents.swift
@@ -0,0 +1,23 @@
+// Возведение в степень
+func pow (_ x: Double, _ n: Double) -> Double {
+    if (n == 1) {
+        return x
+    } else {
+        return x * pow(x, n - 1)
+    }
+}
+
+func rightTriangle (_ legA: Double, _ legB: Double) -> Double {
+    let legC = ((pow(legA, 2) + pow(legB, 2)).squareRoot()).rounded()
+    print("Гипотинуза триугольника С = \(legC)")
+
+    let areaTriangle = ((legA * legB) / 2).rounded()
+    print("Площадь триугольника равна S = \(areaTriangle)")
+
+    let perimeterTriangle = (legA + legB + legC).rounded()
+    print("Периметр триугольника P = \(perimeterTriangle)")
+    
+    return 0
+}
+
+var a = rightTriangle(15, 20)
